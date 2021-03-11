6125783eb0d94fd85e1fedc0e67f7ee0757768f8
Venicyus <venicyusti@gmail.com>
2021-03-11 00:08:40 -0300

    instação e configuração do boostrap

--- src/components/View/index.tsx
+++ src/components/View/index.tsx
@@ -1,22 +1,26 @@
 import React, { ReactElement, ReactNode } from 'react';
 import Head from 'next/head';
 
-import styles from './styles.module.css';
+import SpinnerComponent from './Spinner';
+import styles from './styles.module.scss';
 
 type Props = {
   title: string;
   children: ReactNode;
 };
 
-const ViewComponent = ({ title, children }: Props): ReactElement => (
-  <>
-    <Head>
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>{title}</title>
-    </Head>
-    <div className={styles.container}>{children}</div>
-  </>
-);
+const ViewComponent = ({ title, children }: Props): ReactElement => {
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{title}</title>
+      </Head>
 
+      <SpinnerComponent show={false} />
+      <div className={styles.container}>{children}</div>
+    </>
+  );
+};
 export default ViewComponent;
